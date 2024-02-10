4f12f62117364d6a1a4830a6c29291a75c712b50
shakhbozbekusmonov <miracle_0319@bk.ru>
2024-02-11 00:27:35 +0500

    Initial commit

---
+++ config/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import path, include
+import debug_toolbar
+
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path('', include('store.urls')),
+    path("__debug__/", include(debug_toolbar.urls)),
+]
