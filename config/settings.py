aefda956f54f59ab89b7344d142931dd2c3400cb
shakhbozbekusmonov <miracle_0319@bk.ru>
2024-02-12 00:27:09 +0500

    build: LikedItem model

--- config/settings.py
+++ config/settings.py
@@ -31,6 +31,7 @@ INSTALLED_APPS = [
     # LOCAL APPS
     "store",
     "tags",
+    "likes",
 ]
 
 MIDDLEWARE = [
--- likes/models.py
+++ likes/models.py
@@ -1,15 +1,12 @@
 from django.contrib.contenttypes.fields import GenericForeignKey
 from django.contrib.contenttypes.models import ContentType
 from django.db import models
+from django.contrib.auth.models import User
 
 
-class Tag(models.Model):
-    label = models.CharField(max_length=255)
-
-
-class TagItem(models.Model):
+class LikedItem(models.Model):
     object_id = models.PositiveIntegerField()
 
-    tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
     content_object = GenericForeignKey()
